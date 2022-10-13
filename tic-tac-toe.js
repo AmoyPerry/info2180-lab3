15722117d5c76c33f3fbcaf670303308a8a619eb
Amoy Perry <amoyperry123@gmail.com>
2022-10-13 10:18:38 -0500

    Exercise 2 completed for the javascript.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,29 @@
 "use strict";
+let statArr=[]
+function clicker(param)
+ {
+    if (statArr.length==0 || statArr=="O")
+    {
+       param.innerHTML="X"
+       statArr[0]="X"
+       param.classList.add("X")
+       
+    }
+    else if (statArr==("X"))
+    {
+        param.innerHTML="O"
+        statArr[0]="O"
+        param.classList.add("O")
+        
+    }
+ }
 
 window.onload=function (){
     let myDiv=document.getElementById("board").children
-    for (let count=0;count<myDiv.length;count++){
+
+    for (let count=0;count<myDiv.length;count++)
+    {
         myDiv[count].className="square"
+        myDiv[count].setAttribute("onclick","clicker(this)")
     }
 }
