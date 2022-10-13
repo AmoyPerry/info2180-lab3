1f9998bce7d1f6af58c8bf92a29b708571f66a8b
Amoy Perry <amoyperry123@gmail.com>
2022-10-13 10:44:51 -0500

    Exercise 3 completed for the javascript.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,29 +1,41 @@
 "use strict";
-let statArr=[]
+let statArr = []
 function clicker(param)
  {
-    if (statArr.length==0 || statArr=="O")
+    if (statArr.length == 0 || statArr == "O")
     {
-       param.innerHTML="X"
-       statArr[0]="X"
+       param.innerHTML = "X"
+       statArr[0] = "X"
        param.classList.add("X")
        
     }
-    else if (statArr==("X"))
+    else if (statArr == ("X"))
     {
-        param.innerHTML="O"
-        statArr[0]="O"
+        param.innerHTML = "O"
+        statArr[0] = "O"
         param.classList.add("O")
         
     }
  }
 
-window.onload=function (){
-    let myDiv=document.getElementById("board").children
+ function addHover (param)
+ {
+    param.classList.add("hover")
+ }
 
-    for (let count=0;count<myDiv.length;count++)
+ function removeHover (param)
+ {
+    param.classList.remove("hover")
+ }
+
+window.onload = function (){//this enables the window to load
+    let myDiv = document.getElementById("board").children//gets the children of the div element with ID = board and stores it in a variable, myDiv
+
+    for (let count = 0; count<myDiv.length; count++)
     {
-        myDiv[count].className="square"
+        myDiv[count].className = "square"
         myDiv[count].setAttribute("onclick","clicker(this)")
+        myDiv[count].setAttribute("onmouseover","addHover(this)")
+        myDiv[count].setAttribute("onmouseout","removeHover(this)")
     }
 }
